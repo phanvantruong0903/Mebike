24560524ebc3ea3f2bc4407a2243716e8177d0d6
Truongle68 <truonglephuc11@gmail.com>
2025-12-25 10:40:06 +0700

    feat: edit, dockerfile, update rental service database configuration and health check

--- apps/rental-service/src/app/app.module.ts
+++ apps/rental-service/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RentalModule } from '../modules/rental/rental.module';
+import { HealthController } from '../health/health.controller';
 
 @Module({
   imports: [RentalModule],
-  controllers: [AppController],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
