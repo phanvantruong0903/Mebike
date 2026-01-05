02bcd9ec03821ecaf8f8f9071a5c374d3aa84236
Truongle68 <truonglephuc11@gmail.com>
2026-01-05 19:36:10 +0700

    feat: add eslint, jest, and swc configurations, and define new package and subscription dtos

--- common/src/lib/schema/request/subscription/graphql/GetSubscriptionListInput.ts
+++ common/src/lib/schema/request/subscription/graphql/GetSubscriptionListInput.ts
@@ -1,5 +1,5 @@
 import { InputType } from '@nestjs/graphql';
-import { PaginationInput } from 'src/lib/graphql';
+import { PaginationInput } from '../../../../graphql/abstract-input';
 
 @InputType()
 export class GetSubscriptionListInput extends PaginationInput {}
